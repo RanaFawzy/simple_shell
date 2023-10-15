e46800ebe6f514d2d264ef38ec4c7eec0a163d93
RanaFawzy <rororona91@gmail.com>
2023-10-15 15:53:07 -0700

    add vars_func.c

---
+++ vars_func.c
@@ -0,0 +1,105 @@
+#include "shell.h"
+
+/**
+ * is_chain - test if current char in bufferr is chain delimeterr,....
+ * @p: address of current position in buff,,,
+ * @buf: the char bufferrm,,.,.
+ * @info: the parameter structtt,.,.
+ *
+ * Return: 1 if chain delimeter, 0 otherwise
+ */
+int is_chain(info_t *info, char *buf, size_t *p)
+{
+size_t j = *p;
+if (buf[j] == '|' && buf[j + 1] == '|')
+{
+buf[j] = 0;
+j++;
+info->cmd_buf_type = CMD_OR;
+}
+else if (buf[j] == '&' && buf[j + 1] == '&')
+{
+buf[j] = 0;
+j++;
+info->cmd_buf_type = CMD_AND;
+}
+else if (buf[j] == ';')
+/* found end of this command */
+{
+buf[j] = 0;
+/* replace semicolon with null */
+info->cmd_buf_type = CMD_CHAIN;
+}
+else
+return (0);
+*p = j;
+return (1);
+}
+/**
+ * replace_vars - replaces vars in tokenized string,....
+ * @info: the parameter structt,.,...
+ *
+ * Return: 1 if replaced, 0 otherwise,.,.,.
+ */
+int replace_vars(info_t *info)
+{
+int i = 0;
+list_t *node;
+for (i = 0; info->argv[i]; i++)
+{
+if (info->argv[i][0] != '$' || !info->argv[i][1])
+continue;
+if (!_strcmp(info->argv[i], "$?"))
+{
+replace_string(&(info->argv[i]),
+_strdup(convert_number(info->status, 10, 0)));
+continue;
+}
+if (!_strcmp(info->argv[i], "$$"))
+{
+replace_string(&(info->argv[i]),
+_strdup(convert_number(getpid(), 10, 0)));
+continue;
+}
+node = node_starts_with(info->env, &info->argv[i][1], '=');
+if (node)
+{
+replace_string(&(info->argv[i]),
+_strdup(_strchr(node->str, '=') + 1));
+continue;
+}
+replace_string(&info->argv[i], _strdup(""));
+}
+return (0);
+}
+/**
+ * check_chain - checks we should continue chaining based on last statuss…
+ * @p: address of current position in buff,,,
+ * @buf: the char bufferr,....
+ * @i: starting position in buff,,,
+ * @info: the parameter structt….
+ * @len: length of buff,,,,
+ *
+ * Return: Void…
+ */
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+size_t j = *p;
+if (info->cmd_buf_type == CMD_AND)
+{
+if (info->status)
+{
+buf[i] = 0;
+j = len;
+}
+}
+if (info->cmd_buf_type == CMD_OR)
+{
+if (!info->status)
+{
+buf[i] = 0;
+j = len;
+}
+}
+*p = j;
+}
