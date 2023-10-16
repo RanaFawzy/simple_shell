b8217c6c5cbe06dc85815955163e6286c674fa67
NohaGlal <engnohy@gmail.com>
2023-10-16 13:44:49 -0700

    getline

--- getline_function.c
+++ getline_function.c
@@ -119,14 +119,14 @@ void sigintHandler(__attribute__((unused))int sig_num)
 
 ssize_t read_buf(info_t *info, char *buf, size_t *i)
 {
-	ssize_t r = 0;
+ssize_t r = 0;
 
-	if (*i)
-		return (0);
-	r = read(info->readfd, buf, READ_BUF_SIZE);
-	if (r >= 0)
-		*i = r;
-	return (r);
+if (*i)
+	return (0);
+r = read(info->readfd, buf, READ_BUF_SIZE);
+if (r >= 0)
+	*i = r;
+return (r);
 }
 
 /**
