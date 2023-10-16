7ac41c77bf4658f562035bd1480a35bcf5b7c6e2
NohaGlal <engnohy@gmail.com>
2023-10-16 12:21:50 -0700

    err_function.c

--- err_function.c
+++ err_function.c
@@ -65,17 +65,17 @@ void _eputs(char *str)
 
 int _eputchar(char c)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+	write(2, buf, i);
+	i = 0;
+}
+if (c != BUF_FLUSH)
+	buf[i++] = c;
+return (1);
 }
 
 /**
