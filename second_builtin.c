63d04754d25a877522d8ef4aa85e130b70d3040a
NohaGlal <engnohy@gmail.com>
2023-10-16 12:02:09 -0700

    second builtin

--- second_builtin.c
+++ second_builtin.c
@@ -13,3 +13,51 @@ int _myhistory(info_t *info)
 	return (0);
 }
 
+
+/**
+ * print_alias - function to print  alias str
+ * @node: alias node
+ * Return:  0 ( success)
+ *          1 (failure)
+ */
+
+
+int print_alias(list_t *node)
+{
+char *p = NULL, *a = NULL;
+
+if (node)
+{
+	p = _strchr(node->str, '=');
+	for (a = node->str; a <= p; a++)
+	_putchar(*a);
+	_putchar('\'');
+	_puts(p + 1);
+	_puts("'\n");
+	return (0);
+}
+return (1);
+}
+
+/**
+ * unset_alias - function that set alias to string
+ * @info: the parameter structure
+ * @str: alias str
+ * Return: 0 (on true)
+ *         1 (on failure)
+ */
+int unset_alias(info_t *info, char *str)
+{
+	char *p, c;
+	int ret;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*p = c;
+	return (ret);
+}
