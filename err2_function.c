991a5840442096b0569420d41fa31ee0219d992e
NohaGlal <engnohy@gmail.com>
2023-10-16 13:15:57 -0700

    convert number

--- err2_function.c
+++ err2_function.c
@@ -29,24 +29,24 @@ void print_error(info_t *info, char *estr)
 
 int _erratoi(char *s)
 {
-	int i = 0;
-	unsigned long int result = 0;
+int i = 0;
+unsigned long int result = 0;
 
-	if (*s == '+')
-		s++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  s[i] != '\0'; i++)
+if (*s == '+')
+s++; /* TODO: why does this make main return 255? */
+for (i = 0;  s[i] != '\0'; i++)
+{
+	if (s[i] >= '0' && s[i] <= '9')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
+		result *= 10;
+		result += (s[i] - '0');
+		if (result > INT_MAX)
 			return (-1);
 	}
-	return (result);
+	else
+		return (-1);
+}
+return (result);
 }
 
 
