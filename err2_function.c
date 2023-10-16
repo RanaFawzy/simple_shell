3e2c095075d88098254b1c98ced73ce4b53426ad
NohaGlal <engnohy@gmail.com>
2023-10-16 13:09:44 -0700

    print error

--- err2_function.c
+++ err2_function.c
@@ -96,32 +96,32 @@ char *convert_number(long int num, int base, int flags)
  */
 int print_d(int input, int fd)
 {
-	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
-	unsigned int _abs_, current;
+int (*__putchar)(char) = _putchar;
+int i, count = 0;
+unsigned int _abs_, current;
 
-	if (fd == STDERR_FILENO)
-		__putchar = _eputchar;
-	if (input < 0)
+if (fd == STDERR_FILENO)
+	__putchar = _eputchar;
+if (input < 0)
+{
+	_abs_ = -input;
+	__putchar('-');
+	count++;
+}
+else
+	_abs_ = input;
+current = _abs_;
+for (i = 1000000000; i > 1; i /= 10)
+{
+	if (_abs_ / i)
 	{
-		_abs_ = -input;
-		__putchar('-');
+		__putchar('0' + current / i);
 		count++;
 	}
-	else
-		_abs_ = input;
-	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
-	{
-		if (_abs_ / i)
-		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current %= i;
-	}
-	__putchar('0' + current);
-	count++;
+	current %= i;
+}
+__putchar('0' + current);
+count++;
 
-	return (count);
+return (count);
 }
