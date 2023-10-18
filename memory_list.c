506e3c14be15fc27b93ff4d9e51a50a17c454ac4
RanaFawzy <rororona91@gmail.com>
2023-10-18 03:21:02 -0700

    fixfile memory

--- memory_list.c
+++ memory_list.c
@@ -16,3 +16,19 @@ return (1);
 }
 return (0);
 }
+/**
+ * list_len - determines length of linked list…
+ * @h: pointer first nodee,..
+ * Return: size of list,..
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
